48831ec19fbb9d59ba97d3d8bd6c12dc6a7c3c29
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2025-11-04 20:54:50 -0600

    Create rbac.tf

---
+++ iac/rbac.tf
@@ -0,0 +1,19 @@
+# Setup RBAC to Service Accout External Secrets
+
+resource "kubernetes_cluster_role_binding" "role_tokenreview_binding" {
+  metadata {
+    name = "role-tokenreview-binding"
+  }
+
+  subject {
+    kind      = "ServiceAccount"
+    name      = "external-secrets"
+    namespace = "external-secrets"
+  }
+
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "system:auth-delegator"
+  }
+}
